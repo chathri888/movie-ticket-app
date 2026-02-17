e0f4518e5daec1849d30f01ce27580f0beb04d18
chathri888 <chathri0903@gmail.com>
2026-02-17 16:33:28 +0530

    Create script.js

---
+++ frontend/body {     margin: 0;     font-family: Arial;     background-color: #111;     color: white; }  /* LOGIN */script.js
@@ -0,0 +1,14 @@
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if(user === "admin" && pass === "1234") {
+        window.location.href = "home.html";
+    } else {
+        document.getElementById("error").innerText = "Invalid Username or Password";
+    }
+}
+
+function logout() {
+    window.location.href = "index.html";
+}
